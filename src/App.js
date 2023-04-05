cc1c4560aac988f70d6c106535e2296b191c98e2
Just-Kundai <kundaikhuleya@outlook.com>
2023-04-05 20:24:59 +1000

    starting over

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LogInForm from './Components/Authentication/LogInForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Login Form</h1>
+      <LogInForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Authentication/LogInForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function LogInForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Call login function with email and password
+    console.log('Logging in with email:', email, 'and password:', password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LogInForm;
